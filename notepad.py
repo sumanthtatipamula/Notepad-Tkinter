64237ff2dac2b5a89f9e3d965f6699e998db3dcb
Sumanth Tatipamula <sumanthtatipamula999@gmail.com>
2021-06-05 19:22:47 +0530

    Displays filename as window title

--- notepad.py
+++ notepad.py
@@ -1,5 +1,5 @@
 import tkinter
-from tkinter import LEFT,RIGHT,BOTH,ttk
+from tkinter import LEFT,RIGHT,BOTH,END,ttk,messagebox,filedialog
 import tkinter.font as tkFont
 
 #window
@@ -8,6 +8,8 @@ root.resizable(0,0)
 root.geometry('1000x600')
 root.iconbitmap('notepad.ico')
 root.title('Notepad')
+file_name = ''
+prev_length = 0
 
 #Functions
 def full_screen_mode(event):
@@ -31,13 +33,56 @@ def display_option_widgets():
     save_file.grid(row = 0,column=2,padx=10)
     save_to_cloud.grid(row = 0,column=3,padx=(5,10))
     share_file.grid(row = 0,column=4,padx=10)
+    text.pack(expand=True,fill=BOTH,side=LEFT)
+    my_scroll_bar.pack(fill='y',side=RIGHT)
 
+def new_note(event):
+    global file_name,prev_length
+    question = messagebox.askyesno("New Note", "Are you sure you want to start a new note?")
+    if question == 1:
+        file_name = ''
+        root.title('Untitled.txt-Notepad')
+        text.delete("1.0", END)
+        prev_length = 0
+
+def save_note(event):
+    global file_name,prev_length
+    if file_name == '':
+        file_name = filedialog.asksaveasfilename(initialdir="./", title="Save Note", filetypes=(("Text Files", "*.txt"), ("All Files", "*.*")))
+    if(file_name !=''):
+        file_name = file_name.split("/")[-1]
+        root.title(file_name+"-Notepad")
+        with open(file_name, 'w') as f:
+            f.write(text.get("1.0", END))
+        prev_length = len(text.get("1.0",END))
+
+
+def open_note(event):
+    global file_name,prev_length
+    open_name = filedialog.askopenfilename(initialdir="./", title='Open Note', filetypes=(("Text Files", "*.txt"), ("All Files", "*.*")))
+    if(open_name!= ''):
+        file_name = open_name.split("/")[-1]
+        root.title(file_name+"-Notepad")
+        with open(open_name, 'r') as f:
+            text.delete("1.0", END)
+            t = f.read()
+            text.insert("1.0", t)
+        prev_length = len(text.get("1.0",END))
+
+def note_changed(event):
+    global prev_length
+    if(prev_length!=len(text.get("1.0",END))):
+        prev_length = len(text.get("1.0",END))
+        if(file_name ==''):
+            root.title('*Untitled.txt-Notepad')
+        else:
+            root.title('*'+file_name+'-Notepad')    
 
 
 #Colors and Fonts
-options_frame_color = '#b6d7b9' #'#9abd97' 
+options_frame_color = '#b6d7b9'
 root_color = '#d0f1bf'
-text_box_color = '#d4f2db'#'#dbd56e'
+text_box_color = '#d4f2db'
 root.config(bg = root_color)
 my_font = tkFont.Font(family="Comic Sans MS", size=12)
 style = ttk.Style()
@@ -45,26 +90,21 @@ style.theme_use('clam')
 style.configure("Vertical.TScrollbar", gripcount=10, lightcolor="#8cffda",
                 troughcolor="#cef7a0", bordercolor='#7d7c84', arrowcolor="#de8f6e",darkcolor='#2d93ad',arrowsize=18,sliderlength = 5,background='#2d93ad')
 
-# hs = ttk.Scrollbar(root, orient="vertical")
-# hs.place(x=5, y=5, width=40,height=100)
-# hs.set(0.2,0.3)
-
-
 #Frames
 options_frame = tkinter.Frame(root,bg= options_frame_color)
 options_frame.pack(pady=(0,10))
 
 new_file_image = tkinter.PhotoImage(file='new_file.png')
 new_file = tkinter.Label(options_frame,image = new_file_image,bg=options_frame_color)
-new_file.bind("<Button-1>", command)
+new_file.bind("<Button-1>", new_note)
 
 open_file_image = tkinter.PhotoImage(file='open.png')
 open_file = tkinter.Label(options_frame,image =open_file_image,bg=options_frame_color)
-open_file.bind("<Button-1>", command)
+open_file.bind("<Button-1>", open_note)
 
 save_file_image = tkinter.PhotoImage(file = 'save.png')
 save_file = tkinter.Label(options_frame,image = save_file_image,bg = options_frame_color)
-save_file.bind("<Button-1>", command)
+save_file.bind("<Button-1>", save_note)
 
 save_to_cloud_image = tkinter.PhotoImage(file = 'cloud.png')
 save_to_cloud = tkinter.Label(options_frame,image=save_to_cloud_image,bg=options_frame_color)
@@ -73,20 +113,20 @@ save_to_cloud.bind("<Button-1>", command)
 share_file_image = tkinter.PhotoImage(file= 'sendemail.png')
 share_file = tkinter.Label(options_frame,image=share_file_image,bg=options_frame_color)
 share_file.bind("<Button-1>", command)
-display_option_widgets()
 
 my_scroll_bar = ttk.Scrollbar(root,orient = 'vertical')
 text = tkinter.Text(root,yscrollcommand = my_scroll_bar.set,font=my_font,bg=text_box_color)
 my_scroll_bar.config(command=text.yview)
-text.pack(expand=True,fill=BOTH,side=LEFT)
-my_scroll_bar.pack(fill='y',side=RIGHT)
+
+display_option_widgets()
 
 #bindings
-#"<Control-s>"
 root.bind_all("<F11>",lambda event: full_screen_mode(event))
 root.bind_all("<Escape>",lambda event: root.attributes("-fullscreen",False))
-# three_dots.bind('<Enter>',lambda event:  display_options_frame(event))
-# options_frame.bind('<Leave>',lambda event: hide_options_frame(event))
+root.bind_all("<Control-s>",save_note)
+root.bind_all("<Control-n>",new_note)
+root.bind_all("<Control-o>",open_note)
+text.bind("<Key>",note_changed)
 
 #mainloop
 root.mainloop()

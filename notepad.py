4896e8db58c9e4da193eb3443f1f370e33489082
Sumanth Tatipamula <sumanthtatipamula999@gmail.com>
2021-06-06 08:35:40 +0530

    Provides send to mail access

--- notepad.py
+++ notepad.py
@@ -2,6 +2,7 @@ from __future__ import print_function
 import tkinter
 from tkinter import LEFT,RIGHT,BOTH,END,ttk,messagebox,filedialog
 import tkinter.font as tkFont
+from tkinter import simpledialog
 import os.path
 from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
@@ -9,6 +10,13 @@ from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 from apiclient import errors
 from googleapiclient.http import MediaFileUpload
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.image import MIMEImage
+from email.mime.audio import MIMEAudio
+from email.mime.base import MIMEBase
+import base64
+import re
 import mimetypes
 
 
@@ -20,7 +28,11 @@ root.wm_minsize(1000,600)
 root.iconbitmap('notepad.ico')
 root.title('Notepad')
 file_name = ''
+path = ''
 prev_length = 0
+drive_service = None
+gmail_service = None
+SCOPES = ['https://www.googleapis.com/auth/drive','https://www.googleapis.com/auth/gmail.send']
 # mime_type =  "*/*"#'text/plain'
 
 #Functions
@@ -39,10 +51,9 @@ def hide_options_frame(event):
 def command(event):
     print("hi")
 
-def create_google_drive_service():
+def create_service():
+    global gmail_service,drive_service,SCOPES
     creds = None
-    service = None
-    SCOPES = ['https://www.googleapis.com/auth/drive']
     try:
         if os.path.exists('token.json'):
             creds = Credentials.from_authorized_user_file('token.json', SCOPES)
@@ -55,19 +66,22 @@ def create_google_drive_service():
                 creds = flow.run_local_server(port=0)
             with open('token.json', 'w') as token:
                 token.write(creds.to_json())
-        service = build('drive', 'v3', credentials=creds)
-    except Exception:
-        messagebox.showerror('Google Drive', 'Error occurred while connecting to google drive api')
-    return service
+        drive_service = build('drive','v3', credentials=creds)
+        gmail_service = build('gmail', 'v1',credentials=creds)
+    except Exception as error:
+        messagebox.showerror('Service', 'Error occurred while creating service')
+        print(error)
+    
 
 def insert_file(event):
-  service  = create_google_drive_service()
+  create_service()
+  service = drive_service
   if(service != None):  
     global file_name
     print(file_name)
     mime_type = get_mime_type(file_name)
-    print(mime_type)
-    media_body = MediaFileUpload(file_name, mimetype=mime_type, resumable=True)
+    print(path)
+    media_body = MediaFileUpload(path, mimetype=mime_type, resumable=True)
     body = {
         'name': file_name,
     }
@@ -82,6 +96,73 @@ def insert_file(event):
         messagebox.showerror('Upload File','An error occurred: %s' % error)
         return None
   return; 
+
+def send_to_email(event):
+    create_service()
+    service = gmail_service
+    if(service !=None):
+        to = simpledialog.askstring('Email','Enter To Email Address')
+        validate_email(to)
+        print('path',path)
+        message = create_message_with_attachment(to,'File from Notepad App','This is file is attached from Notepad app',path)
+        send_message(service,"me",message)
+
+def create_message_with_attachment(to, subject, message_text, file):
+  file = r'{}'.format(file)
+  print(file)
+  message = MIMEMultipart()
+  message['to'] = to
+  message['subject'] = subject
+  msg = MIMEText(message_text)
+  message.attach(msg)
+  content_type, encoding = mimetypes.guess_type(file)
+  if content_type is None or encoding is not None:
+    content_type = 'application/octet-stream'  
+  main_type, sub_type = content_type.split('/', 1)
+  print(main_type,content_type,sub_type)
+  try:
+    if main_type == 'text': 
+        fp = open(file, 'rb')
+        msg = MIMEText(main_type, sub_type)
+        msg.set_payload(fp.read())
+        fp.close()
+    elif main_type == 'image':
+        fp = open(file, 'rb')
+        msg = MIMEImage(fp.read(), _subtype=sub_type)
+        fp.close()
+    elif main_type == 'audio':
+        fp = open(file, 'rb')
+        msg = MIMEAudio(fp.read(), _subtype=sub_type)
+        fp.close()
+    else:
+        fp = open(file, 'rb')
+        msg = MIMEBase(main_type, sub_type)
+        msg.set_payload(fp.read())
+        fp.close()
+    filename = os.path.basename(file)
+    msg.add_header('Content-Disposition', 'attachment', filename=filename)
+    message.attach(msg)
+    return {'raw': base64.urlsafe_b64encode(message.as_bytes()).decode()}
+  except Exception as error:
+      messagebox.showerror('Message','Error occurred while creating email message')  
+
+def send_message(service, user_id, message):
+  try:
+    message = (service.users().messages().send(userId=user_id, body=message)
+               .execute())
+    print('Message Id: %s' % message['id'])
+    messagebox.showinfo('Email', 'Sent to email successfully')
+    return message
+  except errors.HttpError as error:
+    print('An error occurred: %s' % error)
+
+def validate_email(email):
+    regex = '^(\w|\.|\_|\-)+[@](\w|\_|\-|\.)+[.]\w{2,3}$'
+    if(re.search(regex, email)):
+        return email; 
+    else:
+        email = simpledialog.askstring('Email','Enter a valid To Email Address')
+        validate_email(email)
 
 def get_mime_type(file_name):
     try:
@@ -108,10 +189,11 @@ def new_note(event):
         prev_length = 0
 
 def save_note(event):
-    global file_name,prev_length
+    global file_name,prev_length,path
     if file_name == '':
         file_name = filedialog.asksaveasfilename(initialdir="./", title="Save Note", filetypes=(("Text Files", "*.txt"), ("All Files", "*.*")))
     if(file_name !=''):
+        path = file_name
         file_name = file_name.split("/")[-1]
         root.title(file_name+"-Notepad")
         with open(file_name, 'w') as f:
@@ -120,9 +202,10 @@ def save_note(event):
 
 
 def open_note(event):
-    global file_name,prev_length
+    global file_name,prev_length,path
     open_name = filedialog.askopenfilename(initialdir="./", title='Open Note', filetypes=(("Text Files", "*.txt"), ("All Files", "*.*")))
     if(open_name!= ''):
+        path = open_name
         file_name = open_name.split("/")[-1]
         root.title(file_name+"-Notepad")
         with open(open_name, 'r') as f:
@@ -174,7 +257,7 @@ save_to_cloud.bind("<Button-1>", insert_file)
 
 share_file_image = tkinter.PhotoImage(file= 'sendemail.png')
 share_file = tkinter.Label(options_frame,image=share_file_image,bg=options_frame_color)
-share_file.bind("<Button-1>", command)
+share_file.bind("<Button-1>", send_to_email)
 
 my_scroll_bar = ttk.Scrollbar(root,orient = 'vertical')
 text = tkinter.Text(root,yscrollcommand = my_scroll_bar.set,font=my_font,bg=text_box_color,undo=True)
